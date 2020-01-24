d3d09df7b2d05b3f23cd5c782cc973314445ba41
Lee Hwanhee <59533451+aiemag@users.noreply.github.com>
2020-01-24 22:27:33 +0900

    Update value_index_order.cpp

--- sort/value_index_order.cpp
+++ sort/value_index_order.cpp
@@ -2,65 +2,127 @@
 
 #define N 100
 
-int a[N];
+int a[N] = { 1, 1, 1, 2, 3 };
 int a_idx[N];
-int n;
+int n = 5;
 
 void order_value_index() {
-	int i, j, min_idx;
+	puts("\order_value_index()");
+	int i, j, max_idx;
 
 	for (i = 0; i < n; i++) {
 		a_idx[i] = i;
 	}
 
 	for (i = 0; i < n - 1 ; i++) {
-		min_idx = i;
+		max_idx = i;
 
-		for (j = i + 1; j < n; j++) {
-			if (a[a_idx[min_idx]] < a[a_idx[j]]) {
-				min_idx = j;
+		for (j = i+1; j < n; j++) {
+			if (a[a_idx[max_idx]] < a[a_idx[j]]) {
+				max_idx = j;
 			}
 		}
 
-		int temp = a_idx[min_idx];
-		a_idx[min_idx] = a_idx[i];
+		int temp = a_idx[max_idx];
+		a_idx[max_idx] = a_idx[i];
 		a_idx[i] = temp;
 	}
 }
 
-void input(void) {
-	int i;
-
-	freopen("./input.txt", "r", stdin);
-	scanf("%d", &n);
+void order_value_index_() {
+	puts("\order_value_index_()");
+	int i, j, max_idx;
 
 	for (i = 0; i < n; i++) {
-		scanf("%d", &a[i]);
+		a_idx[i] = i;
+	}
+
+	for (i = 0; i < n - 1; i++) {
+		max_idx = i;
+
+		for (j = n - 1; j > i; j--) {
+			if (a[a_idx[max_idx]] < a[a_idx[j]]) {
+				max_idx = j;
+			}
+		}
+
+		int temp = a_idx[max_idx];
+		a_idx[max_idx] = a_idx[i];
+		a_idx[i] = temp;
 	}
 }
 
-void process() {
-	order_value_index();
+void order_value_index_asc() {
+	puts("\order_value_index_asc()");
+	int i, j, max_idx;
+
+	for (i = 0; i < n; i++) {
+		a_idx[i] = i;
+	}
+
+	for (i = 0; i < n - 1; i++) {
+		max_idx = i;
+
+		for (j = i + 1; j < n; j++) {
+			if (a[a_idx[max_idx]] < a[a_idx[j]]) {
+				max_idx = j;
+			}
+			else if (a[a_idx[max_idx]] == a[a_idx[j]]) {
+				if (a_idx[max_idx] > a_idx[j]) {
+					max_idx = j;
+				}
+			}
+		}
+
+		int temp = a_idx[max_idx];
+		a_idx[max_idx] = a_idx[i];
+		a_idx[i] = temp;
+	}
+}
+
+void order_value_index_asc_alt() {
+	puts("\order_value_index_asc_alt()");
+	int i, j;
+
+	for (i = 0; i < n; i++) {
+		a_idx[i] = i;
+	}
+
+	for (i = 0; i < n - 1; i++) {
+		for (j = n-1; j > i; j--) {
+			if (a[a_idx[j - 1]] < a[a_idx[j]]) {
+				int temp = a_idx[j];
+				a_idx[j] = a_idx[j-1];
+				a_idx[j-1] = temp;
+			}
+		}
+	}
 }
 
 void output(void) {
 
 	int i;
 
-	printf("idx - ");
-	for (i = 0; i < n; i++) {
-		printf("%d ", a_idx[i]);
-	}
-
-	puts("");
 	printf("val - ");
 	for (i = 0; i < n; i++) {
 		printf("%d ", a[a_idx[i]]);
 	}
+
+	puts("");
+	printf("idx - ");
+	for (i = 0; i < n; i++) {
+		printf("%d ", a_idx[i]);
+	}
+	puts("");
 }
 
 int main(void) {
-	input();
-	process();
+	order_value_index();
+	output();
+	order_value_index_();
+	output();
+	order_value_index_asc();
+	output();
+	order_value_index_asc_alt();
 	output();
 }
