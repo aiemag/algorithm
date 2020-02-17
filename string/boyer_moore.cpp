b6601c16e7ce1cc6cd61f5e227ff6672e7266845
Lee Hwanhee <59533451+aiemag@users.noreply.github.com>
2020-02-17 17:08:37 +0900

    Create boyer_moore.cpp

---
+++ string/boyer_moore.cpp
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+int skip[500];
+
+int mstrlen(char* s) {
+
+	int n = -1;
+	while (s[++n]);
+
+	return n;
+}
+
+void compute_skip(char pattern[], int skip[]){
+	int length = 0, i, j;
+	
+	length = mstrlen(pattern);
+
+	for (i = 0; i < 500; i++){
+		skip[i] = length;
+	}
+
+	for (j = 0; j < length - 1; j++){
+		skip[pattern[j]] = length - 1 - j;
+	}
+}
+
+void do_boyer_moore_horspool(char text[], char pattern[]){
+
+	compute_skip(pattern, skip);
+
+	int i = 0, j, k;
+	int t_len = mstrlen(text);
+	int p_len = mstrlen(pattern);
+
+	while (i <= t_len - p_len){
+		j = p_len - 1;
+		k = i + p_len - 1;
+		
+		while (j >= 0 && pattern[j] == text[k]){
+			j--;
+			k--;
+		}
+
+		if (j == -1){
+			printf("matched index : %d\n", i);
+		}
+
+		i += skip[text[i + p_len - 1]];
+	}
+}
+
+int main(void)
+{
+	char text[1000] = "I am a boy and you are a girl";
+	char pattern[500] = "boy";
+	do_boyer_moore_horspool(text, pattern);
+
+	return 0;
+}
